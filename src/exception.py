cb783f5a3f6e8f3e272d466aed1cf487b52e2f6f
akshayputtaswamy23 <akshayputtaswamy23@gmail.com>
2025-01-05 21:04:22 -0500

    exception_file modified

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,21 @@
-import sys ## sys library will have information of our exceptions
-import logging
+import os
+import sys
 
 def error_message_detail(error,error_detail:sys):
-    #The sys.exc_info() function returns a tuple containing the exception type, exception instance, and traceback object for the currently handled exception
     _,_,exc_tb = error_detail.exc_info()
-    # This gives the file name 
-    file_name = exc_tb.tb_frame.f_code.co_filename 
-    line_no = exc_tb.tb_lineno
-    error_message = "Error occured in the python script[{0}] line number[{1}]".format(file_name,line_no,str(error))
+    filename = exc_tb.tb_frame.f_code.co_filename
+    lineno = exc_tb.tb_lineno
+    error_message = "Error occured in python script name:[{0}] at line number:[{1}] error message [{2}] ".format(filename,lineno,str(error))
     return error_message
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = eval(error_message,error_message_detail = error_detail)
+        self.error_message = error_message_detail(error_message,error_detail= error_detail)
+        
     def __str__(self):
         return self.error_message
+    
+        
     
     
